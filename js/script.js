886c1fda61287cbe13d5bd120928f391ebfe4f8e
GustaSoier <gsampaiosoier0@gmail.com>
2023-08-12 19:22:04 -0300

    Quintal do Pastel
    Arrumei a seção do ifood

--- js/script.js
+++ js/script.js
@@ -38,20 +38,20 @@ mostrarSlide(slideAtual);
 
 
 // menu fixo
-window.onscroll = function(){
-  var top = window.scrollY || document.documentElement.scrollTop;
-  console.log(top);
+// window.onscroll = function(){
+//   var top = window.scrollY || document.documentElement.scrollTop;
+//   console.log(top);
 
-  if(top > 800){
-    console.log("Adicionar meu menu fixo");
-    document.getElementById("topoFixo").classList.add("menu-fixo")
-    document.getElementById("topoFixo").classList.remove("site")
-  }
+//   if(top > 800){
+//     console.log("Adicionar meu menu fixo");
+//     document.getElementById("topoFixo").classList.add("menu-fixo")
+//     document.getElementById("topoFixo").classList.remove("site")
+//   }
 
-  else{
-    console.log("Remover meu menu fixo");
-    document.getElementById("topoFixo").classList.remove("menu-fixo")
-    document.getElementById("topoFixo").classList.add("site")
-  }
-}
+//   else{
+//     console.log("Remover meu menu fixo");
+//     document.getElementById("topoFixo").classList.remove("menu-fixo")
+//     document.getElementById("topoFixo").classList.add("site")
+//   }
+// }
 
